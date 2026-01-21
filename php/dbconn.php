53f831879a0578131111dba30ef802cd00ef5ab0
Amir Sanni <amirsanni@gmail.com>
2026-01-21 09:23:51 +0100

    Use .env for db connection

--- php/dbconn.php
+++ php/dbconn.php
@@ -6,7 +6,19 @@
  * @date 05-Jan-2017
  */
 
-$db = new PDO('mysql:host=localhost;dbname=calendar;charset=utf8mb4', 
-        'root', 
-        '', 
-        [PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+require_once '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
+
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+
+$db = new PDO(
+        "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4",
+        $db_user,
+        $db_pass,
+        [PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
