3a651091d63f3293194e23c24cad3f563f0134c9
Amir Sanni <amirsanni@gmail.com>
2026-01-21 09:25:12 +0100

    Use  super global

--- php/dbconn.php
+++ php/dbconn.php
@@ -11,10 +11,10 @@ require_once '../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
-$db_host = getenv('DB_HOST');
-$db_name = getenv('DB_NAME');
-$db_user = getenv('DB_USER');
-$db_pass = getenv('DB_PASS');
+$db_host = $_ENV['DB_HOST'];
+$db_name = $_ENV['DB_NAME'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
 
 $db = new PDO(
         "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4",
